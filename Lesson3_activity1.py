30eaa263d870580c29fb4acbd00ba0817a18a2bc
jamessitx <billygilltx@proton.me>
2025-01-26 09:30:33 -0600

    completed

---
+++ Lesson3_activity1.py
@@ -0,0 +1,9 @@
+tree1=47
+tree2=93
+tree3=59
+tree4=24
+tree5=82
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of all the trees is:", sum)
+average=sum/5
+print("The average height of all the trees is:", average)
