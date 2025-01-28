8c10b3ad3dcbd92c82c17e17969c32b1a6e6f12b
jamessitx <billygilltx@proton.me>
2025-01-28 17:17:54 -0600

    completed

---
+++ Lesson5_Activity1.py
@@ -0,0 +1,15 @@
+number1 = 34
+number2 = 56
+number3 = 89
+if number1 and number2 and number3:
+    print("The numbers are equal")
+else:
+    print("The numbers are unequal")
+
+number6 = 23
+number5 = 45
+number4 = 12
+if number4 or number5 or number6:
+    print("The numbers have a value")
+else:
+    print("The numbers do not have a value")
