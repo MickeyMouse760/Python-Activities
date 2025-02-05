d3ef5a38ae9c552c95ac2648b00a227118d2096b
jamessitx <billygilltx@proton.me>
2025-02-04 18:58:58 -0600

    completed

--- Lesson6_Activity1.py
+++ Lesson6_Activity1.py
@@ -1,7 +1,6 @@
-num1 = 34
-num2 = 45
-num3 = 56
-num4 = 67
-num5 = 0
-num5 = (num4 * num1)+num2/num3-num2//num4
-print(num5)
+
+x = int(input("Inter a number"))
+if type(x) is int:
+    print("The variable is an integer")
+else:
+    print("The variable isn't an integer")
---
+++ Lesson7_Activity1.py
@@ -0,0 +1,7 @@
+num1 = 34
+num2 = 45
+num3 = 56
+num4 = 67
+num5 = 0
+num5 = (num4 * num1)+num2/num3-num2//num4
+print(num5)
