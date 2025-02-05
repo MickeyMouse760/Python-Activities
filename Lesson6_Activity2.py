71be546a7c0b973b8105cc12858717418b4a4d5b
jamessitx <billygilltx@proton.me>
2025-02-04 18:47:29 -0600

    completed

---
+++ Lesson6_Activity2.py
@@ -0,0 +1,8 @@
+print("Enter a number:")
+num1 = int(input())
+print("Enter another number:")
+num2 = int(input())
+if num1%num2==0:
+    print("You can divide the numbers.")
+else:
+    print("You cannot divide the numbers.")
