affa2a32e337ac4ea090f135ffdb45827569215c
MickeyMouse760 <billygilltx@proton.me>
2025-04-02 16:25:24 -0500

    Add files via upload

---
+++ lesson19_activity1.py
@@ -0,0 +1,6 @@
+from datetime import date,time,datetime
+today = date.today()
+now = datetime.now()
+print("todays date:", today)
+print("\ntime and date:", now)
+print("\nDate components:", today.year, today.month, today.day)
