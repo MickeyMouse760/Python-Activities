e45518aa138c9a25cf7af97cd19e0077123a95ce
MickeyMouse760 <billygilltx@proton.me>
2025-04-04 16:21:08 -0500

    Add files via upload

---
+++ lesson20_activity1.py
@@ -0,0 +1,10 @@
+emptylist = []
+print(emptylist)
+list = ['hello','bye', 46]
+print(list)
+mult_list = [1, 2, 3]*3
+print(mult_list)
+reverse = [4, 5, 6, 7, 8]
+print("before reversing:", reverse)
+reverse = reverse[::-1]
+print("after reversing:", reverse)
