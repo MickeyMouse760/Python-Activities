8cb9cf9db3d684b6e941c6203483956f16c26c64
MickeyMouse760 <billygilltx@proton.me>
2025-04-25 16:21:09 -0500

    Add files via upload

---
+++ lesson23_activity1.py
@@ -0,0 +1,12 @@
+st = {1,2,3,4,5}
+print(st)
+st = {6, 'hi', False, 1.23}
+print(st)
+st = {1,2,3,4,3,2}
+print(st)
+st = set(['1', '2', '3'])
+print(st)
+st = {0,1,2,3,4,5,6}
+print("Original set:",st)
+st.pop()
+print("After removing first element:",st)
