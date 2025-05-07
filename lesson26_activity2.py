5f97458bfd445c11ed0415c8332fe792f5c45948
MickeyMouse760 <billygilltx@proton.me>
2025-05-07 16:26:20 -0500

    Add files via upload

---
+++ lesson26_activity2.py
@@ -0,0 +1,7 @@
+class car:
+    def __init__(self, speed, mileage):
+        self.speed = speed
+        self.mileage = mileage
+ob = car(215, 62)
+print("Speed:", ob.speed)
+print("Mileage:", ob.mileage)
