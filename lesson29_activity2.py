deb105080936729d56f52572d474d12649f76d74
MickeyMouse760 <billygilltx@proton.me>
2025-05-18 11:56:31 -0500

    Add files via upload

---
+++ lesson29_activity2.py
@@ -0,0 +1,8 @@
+class price():
+    def __init__(self, maxprice):
+        self.maxprice = maxprice
+ob = price(900)
+print("The max price is:",ob.maxprice)
+ob = price(1000)
+print("The max price is:",ob.maxprice)
+print("The max price is:",ob.maxprice)
